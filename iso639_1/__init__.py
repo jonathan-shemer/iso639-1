fc23816d0b444573813c723b7f79bc2838839e49
jonathan <jonathan@upstay.tech>
2019-04-23 13:03:54 +0300

    setup support

--- iso639_1/__init__.py
+++ iso639_1/__init__.py
@@ -1,3 +1,5 @@
 from .language import Language as language
 
 __all__ = ["language"]
+
+__version__ = '1.0'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='iso639_1',
+      version='1.0',
+      description='Enum for ISO 639-1 (languages)',
+      url='https://github.com/jonathan-shemer/iso639-1',
+      author='Jonathan Shemer',
+      license='GNUv3',
+      packages=['iso639_1'],
+      install_requires=[])
